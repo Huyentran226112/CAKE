44233cc27c72d98b2e4464228fdfa6c33f0cdd7e
nguyenhuukhuong27102000@gmail.com <nguyenhuukhuong27102000@gmail.com>
2023-07-26 11:53:32 +0700

    update-layout

--- resources/views/admin/include/sidebar.blade.php
+++ resources/views/admin/include/sidebar.blade.php
@@ -7,6 +7,56 @@
                     <a href="index.html"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
                 </li> -->
                 <li class="menu-title">UI elements</li><!-- /.menu-title -->
+                <!-- Category -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"><i class="menu-icon fa fa-list-alt"></i>Category</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-tasks"></i><a href="">Category</a></li>
+                        <li><i class="fa fa-trash-o"></i><a href="">Trash</a></li>
+                    </ul>
+                </li>
+
+                <!-- Product -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"><i class="menu-icon fa fa-fire"></i>Product</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-puzzle-piece"></i><a href="">Product</a></li>
+                        <li><i class="fa fa-trash-o"></i><a href="">Trash</a></li>
+                    </ul>
+                </li>
+                
+                <!-- Customer -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"><i class="menu-icon fa fa-users"></i>Customer</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-user"></i><a href="">Customer</a></li>
+                        <li><i class="fa fa-trash-o"></i><a href="">Trash</a></li>
+                    </ul>
+                </li>
+
+                <!-- Order -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"><i class="menu-icon fa fa-shopping-cart"></i>Order</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-shopping-cart"></i><a href="">Order</a></li>
+                        <li><i class="fa fa-trash-o"></i><a href="">Order Trash</a></li>
+                        <li><i class="fa fa-trash-o"></i><a href="">OrderDetail Trash</a></li>
+                    </ul>
+                </li>
+
+                <!-- User -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"><i class="menu-icon fa ti-user"></i>User</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-user"></i><a href="">User</a></li>
+                        <li><i class="fa fa-users"></i><a href="">Group User</a></li>
+                    </ul>
+                </li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </nav>
